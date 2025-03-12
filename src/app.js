d1007bdec6b8913a135e4cf95103fc755df918d0
Ruben7ctrl <rubencabellomiguel@gmail.com>
2025-03-12 12:50:50 +0000

    Excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function (){
+  const boton = document.querySelector(".btn");
+  const excuseCrtl = document.querySelector(".generator");
+  const phrase = {
+    who: ['Harry Potter ', 'Hermione Granger ', 'Ron Weasley ', 'Draco Malfoy ', 'Dobby ', 'Luna Lovegood ', 'Neville Longbotton ', 'Albus Dumbledore ', 'Lord Voldemort '],
+    action: ['ate ', 'peed ', 'crushed ', 'broke ', 'shit ', 'stole '],
+    what: ['my homework ', 'my wand ', 'the Weasley car ', 'my broom ', 'my Marauder´s Map ', 'my chemistry book '],
+    when: ['before the journey ', 'when I was in my potions class ', 'while flying ', 'during my training ', 'while I was studying ', 'when i was in the battle. ', 'while i was fighting '],
+  }
+  console.log(phrase["who"][0])
+  
+  const whoDidWhat = arr => arr[Math.floor(Math.random() * (arr.length))];
+  const rndm = () => {
+    const subject = whoDidWhat(phrase["who"]);
+    const verb = whoDidWhat(phrase["action"]);
+    const atributte = whoDidWhat(phrase["what"]);
+    const place = whoDidWhat(phrase["when"]);
+    return `${subject} ${verb} ${atributte} ${place}`
+  }
 
+  let currentExcuse;
+  const conectHtml = () => {
+    excuseCrtl.classList.remove("hidden");
+    document.getElementById("excuse").innerHTML = rndm();
+    currentExcuse = document.getElementById("excuse").innerHTML;
+    return currentExcuse;
+  }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  boton.addEventListener("click", conectHtml) // btn.onclick = conectHtml // Son dos formass de ejecitar el elemento del boton (click) en JS
+ 
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+}
